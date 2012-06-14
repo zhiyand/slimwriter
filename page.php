fc66fa414ef083cc5de1f0f1deb4a35d40b5b904
Duan Zhiyan <dzy0451@gmail.com>
2012-06-15 00:20:30 +0800

    featured image for pages

--- page.php
+++ page.php
@@ -1,11 +1,18 @@
 <?php get_header();?>
 
-<?php if(have_posts()) : while(have_posts()) : the_post(); ?>
+<?php if(have_posts()) : while(have_posts()) : the_post();
+$nail = get_the_post_thumbnail( get_the_ID(),  'featured-big');
+?>
 
 <header>
 	<h1><?php the_title();?></h1>
 </header>
 <article>
+	<?php if($nail):?>
+
+	<section class="featured-img"><?php echo $nail; ?></section>
+
+	<?php endif; // nail ?>
 	<?php the_content();?>
 	<?php wp_link_pages( array( 'before' => '<div class="page-link"><span>' . __( 'Pages:', 'slimcoder' ) . '</span>', 'after' => '</div>' ) ); ?>
 	<ul><?php wp_list_pages("title_li=&child_of=".get_the_ID()); ?></ul>
