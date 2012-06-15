c52ee4bb9d84e01039e141bbe9963484665a1768
Duan Zhiyan <dzy0451@gmail.com>
2012-06-15 13:15:00 +0800

    add 404 page

---
+++ 404.php
@@ -0,0 +1,14 @@
+<?php get_header();?>
+
+<header>
+	<h1>404 - Nothing Found</h1>
+</header>
+<article>
+	<p>The content you're looking for is probably deleted or does not exist. Sorry, buddy.</p>
+	<p>Maybe you can try to search the site using the following form.</p>
+	<?php get_search_form();?>
+</article>
+
+<?php get_sidebar();?>
+
+<?php get_footer();?>
