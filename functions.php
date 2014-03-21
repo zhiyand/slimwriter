73ebc6e80c2b60dfefa87c2b92bfbe89e40deb88
Duan Zhiyan <dzy0451@gmail.com>
2014-03-21 16:17:49 +0800

    gallery styling

--- functions.php
+++ functions.php
@@ -17,6 +17,7 @@ class SlimWriterTheme{
         'the_password_form',
         'embed_oembed_html',
         'video_embed_html',
+        array('post_gallery', 10, 2),
     );
     private $opt = null;
 
@@ -46,6 +47,9 @@ class SlimWriterTheme{
         add_theme_support( 'automatic-feed-links' );
         add_image_size('-slimwriter-featured-big', 700, 250, true);
         add_image_size('-slimwriter-featured-small', 140, 90, true);
+        add_image_size('-slimwriter-gallery-full', 660, 408, true);
+        add_image_size('-slimwriter-gallery-medium', 330, 204, true);
+        add_image_size('-slimwriter-gallery-small', 220, 136, true);
 
         load_theme_textdomain('slimwriter', get_template_directory() . '/languages');
 
@@ -138,6 +142,164 @@ class SlimWriterTheme{
     }
     function _video_embed($html){
         return '<div class="video-container">' . $html . '</div>';
+    }
+    /**
+     * The Gallery shortcode.
+     *
+     * This implements the functionality of the Gallery Shortcode for displaying
+     * WordPress images on a post.
+     *
+     * @param array $attr Attributes of the shortcode.
+     * @return string HTML content to display gallery.
+     */
+    function post_gallery($output, $attr) {
+
+        $post = get_post();
+
+        static $instance = 0;
+        $instance++;
+
+        if ( ! empty( $attr['ids'] ) ) {
+            // 'ids' is explicitly ordered, unless you specify otherwise.
+            if ( empty( $attr['orderby'] ) )
+                $attr['orderby'] = 'post__in';
+            $attr['include'] = $attr['ids'];
+        }
+
+        // We're trusting author input, so let's at least make sure it looks like a valid orderby statement
+        if ( isset( $attr['orderby'] ) ) {
+            $attr['orderby'] = sanitize_sql_orderby( $attr['orderby'] );
+            if ( !$attr['orderby'] )
+                unset( $attr['orderby'] );
+        }
+
+        extract(shortcode_atts(array(
+            'order'      => 'ASC',
+            'orderby'    => 'menu_order ID',
+            'id'         => $post ? $post->ID : 0,
+            'itemtag'    => 'div',
+            'icontag'    => 'div',
+            'captiontag' => 'p',
+            'columns'    => 2,
+            'size'       => '-slimwriter-gallery-medium',
+            'include'    => '',
+            'exclude'    => '',
+            'link'       => ''
+        ), $attr, 'gallery'));
+
+        $columns = intval($columns);
+        $span = 6;
+        switch($columns){
+            case 1:
+                $span = 12;
+            $size = '-slimwriter-gallery-full'; break;
+            case 2:
+                $span = 6;
+            $size = '-slimwriter-gallery-medium'; break;
+            case 3:
+                $span = 4;
+            $size = '-slimwriter-gallery-small'; break;
+            case 4:
+            default:
+                $span = 4;
+            $columns = 3;
+            $size = '-slimwriter-gallery-small'; break;
+
+        }
+
+        $id = intval($id);
+        if ( 'RAND' == $order )
+            $orderby = 'none';
+
+        if ( !empty($include) ) {
+            $_attachments = get_posts( array('include' => $include, 'post_status' => 'inherit', 'post_type' => 'attachment', 'post_mime_type' => 'image', 'order' => $order, 'orderby' => $orderby) );
+
+            $attachments = array();
+            foreach ( $_attachments as $key => $val ) {
+                $attachments[$val->ID] = $_attachments[$key];
+            }
+        } elseif ( !empty($exclude) ) {
+            $attachments = get_children( array('post_parent' => $id, 'exclude' => $exclude, 'post_status' => 'inherit', 'post_type' => 'attachment', 'post_mime_type' => 'image', 'order' => $order, 'orderby' => $orderby) );
+        } else {
+            $attachments = get_children( array('post_parent' => $id, 'post_status' => 'inherit', 'post_type' => 'attachment', 'post_mime_type' => 'image', 'order' => $order, 'orderby' => $orderby) );
+        }
+
+        if ( empty($attachments) )
+            return '';
+
+        if ( is_feed() ) {
+            $output = "\n";
+            foreach ( $attachments as $att_id => $attachment )
+                $output .= wp_get_attachment_link($att_id, $size, true) . "\n";
+            return $output;
+        }
+
+        $itemtag = tag_escape($itemtag);
+        $captiontag = tag_escape($captiontag);
+        $icontag = tag_escape($icontag);
+        $valid_tags = wp_kses_allowed_html( 'post' );
+        if ( ! isset( $valid_tags[ $itemtag ] ) )
+            $itemtag = 'dl';
+        if ( ! isset( $valid_tags[ $captiontag ] ) )
+            $captiontag = 'dd';
+        if ( ! isset( $valid_tags[ $icontag ] ) )
+            $icontag = 'dt';
+
+        $itemwidth = $columns > 0 ? floor(100/$columns) : 100;
+        $float = is_rtl() ? 'right' : 'left';
+
+        $selector = "gallery-$instance";
+
+        $gallery_style = $gallery_div = '';
+
+        $size_class = sanitize_html_class( $size );
+        $gallery_div = "<div id='$selector' class='gallery galleryid-{$id} gallery-columns-{$columns} gallery-size-{$size_class}'>";
+
+        $output = $gallery_div;
+
+        $i = 0;
+        foreach ( $attachments as $id => $attachment ) {
+
+            if ( $columns > 0 && $i % $columns == 0 )
+                $output .= '<div class="row">';
+
+            if ( ! empty( $link ) && 'file' === $link )
+                $image_output = wp_get_attachment_link( $id, $size, false, false );
+            elseif ( ! empty( $link ) && 'none' === $link )
+                $image_output = wp_get_attachment_image( $id, $size, false );
+            else
+                $image_output = wp_get_attachment_link( $id, $size, true, false );
+
+            $image_meta  = wp_get_attachment_metadata( $id );
+
+            $orientation = '';
+            if ( isset( $image_meta['height'], $image_meta['width'] ) )
+                $orientation = ( $image_meta['height'] > $image_meta['width'] ) ? 'portrait' : 'landscape';
+
+            $output .= "<{$itemtag} class='gallery-item col-md-{$span}'>";
+            $output .= "
+                <{$icontag} class='gallery-icon {$orientation}'>
+                    $image_output
+                </{$icontag}>";
+            if ( $captiontag && trim($attachment->post_excerpt) ) {
+                $output .= "
+                    <{$captiontag} class='wp-caption-text gallery-caption'>
+                    " . wptexturize($attachment->post_excerpt) . "
+                    </{$captiontag}>";
+            }
+            $output .= "</{$itemtag}>";
+            if ( $columns > 0 && ++$i % $columns == 0 ){
+                $output .= '</div><!-- .row -->';
+            }
+        }
+        if($i % $columns != 0){
+                $output .= '</div><!-- .row -->';
+        }
+
+        $output .= "
+            </div>\n";
+
+        return $output;
     }
 
     static $SIDEBARS = array(
